b6776083cb76a5da63e7c3190bcd51ebd5b3bdcc
shahf215 <114938114+shahf215@users.noreply.github.com>
2023-04-30 21:59:08 -0400

    Test Git - Eclipse Connection

---
+++ Prob&Stat CSV Program/src/ChangeNameLater.java
@@ -0,0 +1,38 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+public class ChangeNameLater {
+	//change function to be more complicated
+	//this is for project 2
+
+	public static void main(String args[]) {
+	File CSVTester = new File("FunctionResults.csv");
+	
+	try {
+	
+		BufferedWriter bw = new BufferedWriter(new FileWriter(CSVTester));
+		
+		String title = "labelx, labely";
+		bw.write(title);
+		
+		for(int i = 1; i <= 100; i++) {
+			
+			int y = (2*i) + 5;
+			
+			bw.newLine();
+			bw.write(i + "," + y);
+			
+		}
+	
+		
+		bw.close();
+		System.out.println("hi");
+		
+	}
+	
+	catch(Exception e) {
+		
+	}
+}
+}
